1a16f43f82aba2fa41978dcba0c21be978979f78
Fred Chen <fredchen1994@outlook.com>
2020-06-10 21:56:44 +0100

    dom

---
+++ RoundTV/index.js
@@ -0,0 +1,13 @@
+/* The document object model (DOM) */
+
+// root element, i.e. this html file
+document;
+
+// the root html element
+document.firstElementChild;
+
+// the head element
+document.firstElementChild.firstElementChild;
+
+// the body. Can be saved inside a var
+var body = document.firstElementChild.lastElementChild;

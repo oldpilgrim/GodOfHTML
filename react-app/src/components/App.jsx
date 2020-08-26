b89966e6d3c71ffffafe867d64a1d444e6b40e6b
Fred Chen <fredchen1994@outlook.com>
2020-08-26 16:02:52 +0100

    more react

--- react-app/src/components/App.jsx
+++ react-app/src/components/App.jsx
@@ -3,11 +3,22 @@ import React from 'react';
 import Heading from "./Heading";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
     return <div>
         <Heading />
-        <Note />
+
+        {notes.map(note => {
+            return (
+                <Note
+                    key={note.key}
+                    title={note.title}
+                    content={note.content}
+                />
+            );
+        })}
+
         <Footer />
     </div>
 }
